3dcbca08e6cdfdf2174074cec301bac63981fe6f
Matthew M <MatthewFMcAndrews@gmail.com>
2022-12-01 14:41:24 -0600

    solve day 1

---
+++ Day1.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class ElfCalories
+    {
+        public static string MaxCarried() => CalorieCounts().Max().ToString();
+        public static string SumOfTopThree() => CalorieCounts().Take(3).Sum().ToString();
+        private static List<int> CalorieCounts() {
+            var lines = File.ReadAllLines("..\\..\\day-1-elf-calories.txt");
+            var calorieCounts = new List<int>();
+            var elfCalories = 0;
+            foreach (var line in lines)
+            {
+                if (int.TryParse(line, out int calories))
+                {
+                    elfCalories += calories;
+                }
+                else
+                {
+                    calorieCounts.Add(elfCalories);
+                    elfCalories = 0;
+                }
+            }
+            calorieCounts.Sort();
+            calorieCounts.Reverse();
+            return calorieCounts;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AdventOfCode
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace AdventOfCode {
+    class Program {
+        static void Main(string[] args) {
+            try
+            {
+                int dayOfChristmas = int.Parse(args[0]);
+                int challengeNumber = int.Parse(args[1]);
+                string answer = GetAnswerFor((dayOfChristmas, challengeNumber));
+                Console.WriteLine(answer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
+        public static string GetAnswerFor((int dayOfChristmas, int challengeNumber) id) => id switch
+        {
+            (1, 1) => ElfCalories.MaxCarried(),
+            (1, 2) => ElfCalories.SumOfTopThree(),
+            _ => throw new Exception($"unsupported day of christmas {id.dayOfChristmas}, challenge {id.challengeNumber}."),
+        };
     }
 }
